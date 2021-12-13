09f0c9b0a31c8e8d93d1e5b9cde9348d9f0baccd
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-12-13 16:56:27 +0800

    enemy bullet get destroyed on collision with anything other than the enemy itself, OR when a set time like 5 sec runs out

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+    public float timeLeft = 5;
     public float speed = 20f;
     public Rigidbody2D rigidBod;
 
@@ -20,6 +21,18 @@ public class EnemyBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        // Debug.Log(hitInfo.name);
+
+        if (hitInfo.name != "Enemy")
+            Destroy(gameObject);
     }
 }
