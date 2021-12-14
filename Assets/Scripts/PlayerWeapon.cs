969d09f38a22eebf08864ce4eaf7d44c9f67de0b
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-12-14 17:11:33 +0800

    Fixed wonky player shooting

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -7,7 +7,7 @@ public class PlayerWeapon : MonoBehaviour
     [SerializeField] private Transform firepoint;
     [SerializeField] private GameObject playerBullet;
     [SerializeField] private float bulletInterval = 5;
-
+    Vector2 requiredVector;
     private float bulletCooldownTimer;
     public float bulletForce = 20f;
     Vector3 mousePos;
@@ -26,6 +26,8 @@ public class PlayerWeapon : MonoBehaviour
         tempPos.z = 10;
         mousePos = cam.ScreenToWorldPoint(tempPos);
 
+        requiredVector = (Vector2)mousePos - (Vector2)firepoint.position;
+
         if (Input.GetKeyDown("space"))
         {
             if (!GameManager.gameHasEnded)
@@ -37,9 +39,11 @@ public class PlayerWeapon : MonoBehaviour
 
     void shoot()
     {
-        GameObject bullet = Instantiate(playerBullet, firepoint.position, firepoint.rotation);
+        float angle = Mathf.Atan2(requiredVector.y, requiredVector.x) * Mathf.Rad2Deg;
+
+        GameObject bullet = Instantiate(playerBullet, firepoint.position, Quaternion.AngleAxis(angle, Vector3.forward));
         Rigidbody2D rigidBod = bullet.GetComponent<Rigidbody2D>();
-        rigidBod.AddForce((Vector2)mousePos * bulletForce, ForceMode2D.Impulse);
+        rigidBod.AddForce((Vector2)requiredVector * bulletForce, ForceMode2D.Impulse);
 
         bulletCooldownTimer -= Time.deltaTime;
 
